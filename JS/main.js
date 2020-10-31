4fd76af75b49c86d819778ed52bac7f3bbaf6e83
Guy Roberts <guyroberts21@outlook.com>
2020-10-31 18:09:09 +0000

    Add new navbar

--- JS/main.js
+++ JS/main.js
@@ -9,19 +9,30 @@ function toggleLight(btn) {
     btn.classList.add('hidden');
 
     if (btn.classList.contains('light')) {
-      wrapper.className = 'theme-light';
+      wrapper.className = 'theme-dark';
       root.style.setProperty('--color-background', 'hsl(210deg, 30%, 8%)');
       root.style.setProperty('--color-text', 'hsl(0deg, 0%, 100%)');
+      root.style.setProperty('--color-primary', 'hsl(333deg, 100%, 52%)');
+      root.style.setProperty('--color-secondary', 'hsl(245deg, 100%, 60%)');
     } else {
-      wrapper.className = 'theme-dark';
+      wrapper.className = 'theme-light';
       root.style.setProperty('--color-background', ' hsl(0deg, 0%, 100%)');
       root.style.setProperty('--color-text', 'hsl(210deg, 30%, 8%)');
+      root.style.setProperty('--color-primary', 'hsl(245deg, 100%, 60%)');
+      root.style.setProperty('--color-secondary', 'hsl(333deg, 100%, 52%)');
     }
     wrapper.className = btn.classList.contains('dark')
-      ? 'theme-light'
-      : 'theme-dark';
+      ? 'theme-dark'
+      : 'theme-light';
   });
 }
 
 const lightBtns = document.querySelectorAll('.light-settings .icon');
 lightBtns.forEach((btn) => toggleLight(btn));
+
+// Top Navigation
+const menu = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-links');
+menu.addEventListener('click', () => {
+  nav.classList.toggle('open-nav');
+});
